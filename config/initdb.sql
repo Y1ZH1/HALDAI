a258d00ee57dc4574ddaac1456040d4ab2a1d3c8
bracy glan <bracy@ngg789>
2024-12-09 22:38:24 +0800

    add uuid

---
+++ config/initdb.sql
@@ -0,0 +1,8 @@
+-- 创建用户信息表
+CREATE TABLE userinfo (
+    uuid VARCHAR(36) PRIMARY KEY NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP NULL
+);
